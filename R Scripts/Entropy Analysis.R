3aad6ae6a4bd1f16e446871ec393431a314baf73
araikes <adam.raikes@gmail.com>
2016-10-31 14:34:00 -0600

    Initial commit for entropy analysis in R

---
+++ R Scripts/Entropy Analysis.R
@@ -0,0 +1,64 @@
+#### COP Data Analysis ####
+# This is a collaborative project between Dr. Eadric Bressel, Adam Raikes, Talin
+# Louder, and Devin Patterson. In this arm of the analysis, I will analyze the
+# raw X and Y COP data to examine the multiscale complexity of single and dual
+# task postural sway on land and water.
+
+# The data was provided to me as a .xlsx file with 4 tabs. Tabs 4 and 5 are the
+# COPx and Y. However, these are untidy data with blank columns to separate
+# conditions. Prior to computing multiscale entropy, this dataset requires
+# substantial cleaning.
+
+# This script is the second phase of analysis. MSE has alreay been computed on
+# the dataset. This analysis will employ a 2x2 repeated measures design. 
+
+# To account for the possibility of nonstationarity in the data, the analysis
+# will look at both raw and detrended COP data.
+
+#### Load libraries ####
+library(tidyverse)
+library(nlme)
+
+#### Load MSE data ####
+mse.x <- read.csv("Data Files/MSE X.csv", header = FALSE)
+mse.y <- read.csv("Data Files/MSE Y.csv", header = FALSE)
+mse.x.detrended <- read.csv("Data Files/Detrended X MSE.csv", header = FALSE)
+mse.y.detrended <- read.csv("Data Files/Detrended Y MSE.csv", header = FALSE)
+
+#### Add subject indicators to each data frame
+mse.x <- mutate(mse.x, Participant = subject.vec)
+mse.y <- mutate(mse.y, Participant = subject.vec)
+mse.x.detrended <- mutate(mse.x.detrended, Participant = subject.vec)
+mse.y.detrended <- mutate(mse.y.detrended, Participant = subject.vec)
+
+#### Reshape to long and reformat the scale value column
+mse.x <- mse.x %>%
+  select(-V1) %>%
+  gather(Scale, ml.mse, -Participant)
+mse.x$Scale <- as.numeric(gsub("V","", mse.x$Scale))
+
+mse.x.detrended <- mse.x.detrended %>%
+  select(-V1) %>%
+  gather(Scale, ml.detrended.mse, -Participant)
+mse.x.detrended$Scale <- as.numeric(gsub("V","", mse.x.detrended$Scale))
+
+mse.y <- mse.y %>%
+  select(-V1) %>%
+  gather(Scale, ap.mse, -Participant)
+mse.y$Scale <- as.numeric(gsub("V","", mse.y$Scale))
+
+mse.y.detrended <- mse.y.detrended %>%
+  select(-V1) %>%
+  gather(Scale, ap.detrended.mse, -Participant)
+mse.y.detrended$Scale <- as.numeric(gsub("V","", mse.y.detrended$Scale))
+
+#### Join data frames in long format ####
+mse.data <- inner_join(mse.x, mse.x.detrended)
+mse.data <- inner_join(mse.data, mse.y)
+mse.data <- inner_join(mse.data, mse.y.detrended)
+
+#### Split participant column ####
+mse.data <- extract(mse.data, Participant, into = c("Subject", "Environment", "Task"),
+                    "([0-2][0-9])([L|W])([S|D])")
+
+
